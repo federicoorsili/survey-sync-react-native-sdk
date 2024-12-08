82bc1243237502efeda4691b3641f3f371b50dd4
Alina Nosovets <alinanosovets6849@gmail.com>
2024-12-08 22:01:57 +0100

    build: add missing dependencies

--- survey-sync-react-native-sdk-1/src/index.tsx
+++ survey-sync-react-native-sdk-1/src/index.tsx
@@ -1,3 +1,27 @@
-export function multiply(a: number, b: number): number {
-  return a * b;
+import { useFonts } from 'expo-font';
+import { ThemeProvider } from './context/ThemeContext';
+import SurveyWrapper from './components/SurveyView/SurveyWrapper';
+
+interface AppProps {
+  surveyAlias: string;
+  onFinishedSurvey?: (respondentId: string) => void;
+  theme?: 'light' | 'dark';
 }
+
+const Survey = ({ surveyAlias, onFinishedSurvey, theme }: AppProps) => {
+  const [fontsLoaded] = useFonts({
+    InterBold: require('./assets/fonts/Inter18pt-Bold.ttf'),
+    InterMedium: require('./assets/fonts/Inter18pt-Medium.ttf'),
+    InterRegular: require('./assets/fonts/Inter18pt-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) return null;
+
+  return (
+    <ThemeProvider customTheme={theme}>
+      <SurveyWrapper alias={surveyAlias} onFinishedSurvey={onFinishedSurvey} />
+    </ThemeProvider>
+  );
+};
+
+export default Survey;
