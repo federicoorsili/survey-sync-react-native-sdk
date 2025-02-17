4118f6637199c48e9269f2fed95926de79d36750
Alina Nosovets <alinanosovets6849@gmail.com>
2025-02-17 12:45:30 +0100

    fix: empty list placeholder

--- src/components/Placeholder/EmptyListPlaceholder.tsx
+++ src/components/Placeholder/EmptyListPlaceholder.tsx
@@ -1,11 +1,25 @@
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const EmptyListPlaceholder = ({ text }: { text: string }) => {
   return (
-    <View>
-      <View>{text}</View>
+    <View style={styles.container}>
+      <Text style={styles.placeholderText}>{text}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    // styling for the outer container
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  placeholderText: {
+    // styling for your text
+    fontSize: 16,
+    color: '#888',
+  },
+});
 
 export default EmptyListPlaceholder;
